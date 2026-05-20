d08e415b1671501ebe6bfb09d53dac6c21f91dac
alfaxad1 <alfaxadder@gmail.com>
2026-05-21 01:09:49 +0300

    restrict CORS origins for enhanced security

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const allowedOrigins = [
-    'http://localhost:3000',
-    'https://adtechhomes.co.ke',
-    'http://adtechhomes.co.ke',
-    // any custom domain where you host the client
-  ];
 
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: true,
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
